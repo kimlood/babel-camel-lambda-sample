a1945e4384fc0208cacd10b372ab2d7154a2bb41
Christophe Pache <christophe.pache@crossing-tech.com>
2014-11-11 21:27:04 +0100

    one solution with babel-camel-lambda

--- src/main/java/io/xtech/babel/lambda/sample/MyRoute.java
+++ src/main/java/io/xtech/babel/lambda/sample/MyRoute.java
@@ -7,6 +7,17 @@ public class MyRoute extends RouteBuilder {
     @Override
     public void configure() {
         //define your route here
-
+        from(source("direct:input")).as(String.class).
+                processBody((str) -> {
+                    final String[] amounts = str.split(",");
+                    int acc = 0;
+                    for (String amount : amounts) {
+                        acc += Integer.parseInt(amount);
+                    }
+                    return acc;
+                }).choice(c -> {
+            c.when(x -> x > 0).to(sink("mock:output1"));
+            c.when(x -> x < 0).to(sink("mock:output2"));
+        });
     }
 }
