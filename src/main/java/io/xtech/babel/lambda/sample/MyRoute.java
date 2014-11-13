1b226e3aa18c128fe446ddd879e69f01be5433cc
Christophe Pache <christophe.pache@crossing-tech.com>
2014-11-13 09:43:38 +0100

    java based solution

--- src/main/java/io/xtech/babel/lambda/sample/MyRoute.java
+++ src/main/java/io/xtech/babel/lambda/sample/MyRoute.java
@@ -6,8 +6,21 @@ public class MyRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-        from(source("file:input")).
         //define your route here
-        to(sink("file:output"));
+        from(source("file:input")).as(String.class).
+                processBody((str) -> {
+                    final String[] amounts = str.split(",");
+                    int acc = 0;
+                    for (String amount : amounts) {
+                        acc += Integer.parseInt(amount.trim());
+                    }
+                    return acc;
+                }).choice(c -> {
+            c.when(x -> x > 0).
+                    as(String.class).to(sink("file:output1"));
+            c.when(x -> x < 0).
+                    processBody((i) -> i + " is negative").to(sink("file:output2"));
+        });
     }
+
 }
--- src/test/java/io/xtech/babel/lambda/MyRouteTest.java
+++ src/test/java/io/xtech/babel/lambda/MyRouteTest.java
@@ -15,6 +15,8 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 import org.junit.Test;
 
+import java.io.File;
+
 public class MyRouteTest {
 
     @Test
@@ -24,15 +26,32 @@ public class MyRouteTest {
         final String input2 = "10,20,-40";
 
         RouteBuilder routeBuilder = new MyRoute();
+        new File("ouput1").mkdirs();
+        new File("output2").mkdirs();
+        File[] files = new File("input").listFiles();
+        for (File file: files){
+            file.delete();
+        }
 
         DefaultCamelContext camelContext = new DefaultCamelContext();
+
+        new org.apache.camel.builder.RouteBuilder(){
+
+            @Override
+            public void configure() throws Exception {
+                from("direct:input").to("file:input");
+                from("file:output1").to("mock:output1");
+                from("file:output2").to("mock:output2");
+            }
+        }.addRoutesToCamelContext(camelContext);
+
         camelContext.addRoutes(routeBuilder.getRouteBuilder());
         camelContext.start();
 
         final MockEndpoint mockEndpoint1 = (MockEndpoint) camelContext.getEndpoint("mock:output1");
         final MockEndpoint mockEndpoint2 = (MockEndpoint) camelContext.getEndpoint("mock:output2");
         mockEndpoint1.expectedBodiesReceived("60");
-        mockEndpoint2.expectedBodiesReceived("-10");
+        mockEndpoint2.expectedBodiesReceived("-10 is negative");
 
         ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
         producerTemplate.sendBody("direct:input", input1);
